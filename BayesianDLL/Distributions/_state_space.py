Fixed a bug with observed parameter gradients in model gradient and implemented the dirichlet distribution state space

BayesianDLL/Distributions/_state_space.py
@@ -105,6 +105,13 @@ class ContinuousPositive(ContinuousSpace):
     def contains(self, state):
         return torch.all(state > 0)
 
-class ContinuousReal (ContinuousSpace):
+class ContinuousReal(ContinuousSpace):
     def contains(self, state):
         return torch.all(torch.isfinite(state))
+
+class ContinuousSimplex(ContinuousSpace):
+    def contains(self, state):
+        between_0_and_1 = torch.all((0 <= state) & (state <= 1))
+        sum = torch.sum(state)
+        sum_to_1 = torch.allclose(sum, torch.ones_like(sum))
+        return between_0_and_1 & sum_to_1

Tests/gaussian_mixture_models/sampling_weights.py
@@ -17,7 +17,7 @@ with Model() as model:
     theta_init = torch.ones(K, dtype=torch.float64)
     theta_init[0], theta_init[1] = 2, 3  # make the initial point not uniform to stabilize the sampling
     theta_init = torch.softmax(theta_init, dim=0)
-    weights = RandomParameter("weights", Dirichlet(alpha), theta_init, sampler="metropolis")
+    weights = RandomParameter("weights", Dirichlet(alpha), theta_init, sampler="nuts", gamma=5)
 
     means = [torch.tensor([-2.0], dtype=torch.float64), torch.tensor([2.0], dtype=torch.float64)]
     covs = [torch.eye(1, dtype=torch.float64) * 0.5 ** 2, torch.eye(1, dtype=torch.float64) * 0.5 ** 2]

Tests/sampling/NUTS_sampling.py
@@ -172,11 +172,11 @@ d = 3
 alpha = torch.tensor([2, 4, 8], dtype=torch.float64)
 distribution = Dirichlet(alpha)
 theta_init = torch.ones(d, dtype=torch.float64)
-theta_init[0], theta_init[1] = 2, 3  # make the initial point not uniform to stabilize the sampling
+theta_init[0], theta_init[1] = 2, -10  # make the initial point in an area with little probability mass to stabilize the sampling
 theta_init = torch.softmax(theta_init, dim=0)
 with Model() as model:
     RandomParameter("sample", distribution, theta_init, sampler="nuts", gamma=5)
-    samples = sample(max(n // 20, 100), 1000)["sample"]
+    samples = sample(max(n // 20, 100), 2000)["sample"]
 def project_to_2d(points):
     v1 = torch.tensor([0.0, 0.0])
     v2 = torch.tensor([1.0, 0.0])