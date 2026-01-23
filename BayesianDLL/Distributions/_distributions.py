Implemented find_MAP and fixed a bug with the distribution _unconstrained_log_prob, where the determinant of the jacobian was zero.

BayesianDLL/Distributions/_distributions.py
@@ -39,7 +39,8 @@ class Distribution(ABC):
 
         x_constrained = self.transform.inverse(x_unconstrained)
         jacobian = self.transform.derivative(x_unconstrained)
-        log_det_abs_jacobian = torch.linalg.slogdet(jacobian).logabsdet
+        # log_det_abs_jacobian = torch.linalg.slogdet(jacobian).logabsdet
+        log_det_abs_jacobian = torch.log(torch.abs(torch.linalg.det(jacobian)) + 1e-8)  # if the determinant is zero, the logabsdet does not go to negative infinity
         return self.log_pdf(x_constrained).squeeze(1) + log_det_abs_jacobian  # (n_samples,)
     
     def _log_prob_grad_unconstrained(self, x_unconstrained):

Tests/bayesian_regression/bayesian_linear_regression.py
@@ -1,7 +1,7 @@
 import torch
 import matplotlib.pyplot as plt
 
-from BayesianDLL.Distributions import Normal, HalfCauchy, Independent
+from BayesianDLL.Distributions import Normal, HalfCauchy
 from BayesianDLL import Model, RandomParameter, ObservedParameter, DeterministicParameter, sample, sample_posterior_predicative
 from BayesianDLL.Evaluation import Graphics
 
@@ -25,10 +25,11 @@ with Model() as linear_model:
     # make the transform for the predicted line
     mu = DeterministicParameter("mu", lambda b, m: m * x + b, lambda b, m: {"slope": x, "intercept": torch.ones_like(x)}, [prior_intercept, prior_slope])
     
-    likelihood = ObservedParameter("likelihood", Independent(Normal(mu, prior_sigma), dims=0), y)  # TODO: cannot use Independent as we do not want to sum over the samples during posterior predicative sampling
+    likelihood = ObservedParameter("likelihood", Normal(mu, prior_sigma), y)
     
     samples = sample(1000, 1000, n_chains=2)
-    posterior_predicative_samples = sample_posterior_predicative(n_samples=20, warmup_length=100, samples_per_step=500, warmup_per_sample=500)
+    # sample posterior predicative does not currently work as likelihood should return multidimensional values (one for every x)
+    # posterior_predicative_samples = sample_posterior_predicative(n_samples=20, warmup_length=100, samples_per_step=500, warmup_per_sample=500)
     # print(posterior_predicative_samples, posterior_predicative_samples["likelihood"].shape)
     # plt.plot(posterior_predicative_samples["likelihood"][:, :, 0].T)
     # plt.show()
@@ -40,8 +41,8 @@ Graphics.plot_model(linear_model)
 plt.figure()
 Graphics.plot_posterior(samples)
 
-plt.figure()
-Graphics.plot_predicative_distribution(posterior_predicative_samples, y, method="kde")
+# plt.figure()
+# Graphics.plot_predicative_distribution(posterior_predicative_samples, y, method="kde")
 
 
 x = x.squeeze()

Tests/gaussian_mixture_models/sampling_dist_params.py
@@ -2,7 +2,7 @@ import torch
 import matplotlib.pyplot as plt
 
 from BayesianDLL.Distributions import Dirichlet, Normal, Mixture, Exponential
-from BayesianDLL import Model, RandomParameter, ObservedParameter, sample
+from BayesianDLL import Model, RandomParameter, ObservedParameter, sample, find_MAP
 from BayesianDLL.Evaluation.Graphics import plot_posterior, plot_model
 
 
@@ -17,7 +17,7 @@ data = torch.cat([
 ])
 
 sampler_params = {"min_step_size": 1e-6, "max_step_size": 10, "delta": 0.6, "gamma": 0.5}
-weight_sampler_params = {"min_step_size": 1e-10, "max_step_size": 10, "delta": 0.6, "gamma": 5}
+weight_sampler_params = {"min_step_size": 1e-6, "max_step_size": 10, "delta": 0.6, "gamma": 0.5, "max_depth": 4}
 
 with Model() as model:
     alpha = 1.5 * torch.ones(K, dtype=torch.float64)
@@ -33,36 +33,44 @@ with Model() as model:
 
     likelihood = ObservedParameter("likelihood", Mixture(components, weights), data.unsqueeze(1))
 
+    plt.figure()
     plot_model(model)
+
+    # make sampling start close to the maximum a posteriori
+    plt.figure()
+    plt.plot(find_MAP(model, lr=1e-2, epochs=350, callback_frequency=35))
+    print(weights.constrained_value)
+    print([mean.constrained_value for mean in means])
+    print([variance.constrained_value for variance in variances])
     plt.show()
 
-    samples = sample(2000, 2000, n_chains=1)
-    samples = {name: value.squeeze(0) for name, value in samples.items()}
+    samples = sample(400, 100, n_chains=2)
 
 plt.figure()
 for k in range(K):
-    plt.hist(samples["weights"][:, k].numpy(), bins=30, density=True, alpha=0.6, label=f"Weight {k}")
+    plt.hist(samples["weights"].flatten(0, 1)[:, k].numpy(), bins=30, density=True, alpha=0.6, label=f"Weight {k}")
 plt.xlabel("Weight value")
 plt.ylabel("Density")
 plt.legend()
 plt.tight_layout()
 
 plt.figure()
-plt.hist(samples["mean1"].numpy(), bins=30, density=True, alpha=0.6, label="Mean 1")
-plt.hist(samples["mean2"].numpy(), bins=30, density=True, alpha=0.6, label="Mean 2")
+plt.hist(samples["mean1"].flatten(0, 1).numpy(), bins=30, density=True, alpha=0.6, label="Mean 1")
+plt.hist(samples["mean2"].flatten(0, 1).numpy(), bins=30, density=True, alpha=0.6, label="Mean 2")
 plt.xlabel("Means")
 plt.ylabel("Density")
 plt.legend()
 plt.tight_layout()
 
 plt.figure()
-plt.hist(samples["variance1"].numpy(), bins=30, density=True, alpha=0.6, label="Variance 1")
-plt.hist(samples["variance2"].numpy(), bins=30, density=True, alpha=0.6, label="Variance 2")
+plt.hist(samples["variance1"].flatten(0, 1).numpy(), bins=30, density=True, alpha=0.6, label="Variance 1")
+plt.hist(samples["variance2"].flatten(0, 1).numpy(), bins=30, density=True, alpha=0.6, label="Variance 2")
 plt.xlabel("Variances")
 plt.ylabel("Density")
 plt.legend()
 plt.tight_layout()
 
-# plot_posterior(samples)
+plt.figure(figsize=(12, 8))
+plot_posterior(samples)
 
 plt.show()